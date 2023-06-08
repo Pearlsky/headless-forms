5481bfbcc4e77e72e7abbaf32f8a3b9155f63f5c
pearlsky <pearlakpan115@gmail.com>
2023-06-08 18:10:35 +0100

    chore(aliases): set up aliases for absolute improrts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+// path import supported by installing @types/node
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+      "@assets": `${path.resolve(__dirname, "./src/assets")}`,
+      "@components": `${path.resolve(__dirname, "./src/components")}`,
+      "@layouts": `${path.resolve(__dirname, "./src/layouts")}`,
+      "@views": `${path.resolve(__dirname, "./src/views")}`,
+      "@types": `${path.resolve(__dirname, "./src/types")}`,
+    },
+  },
+});
